8f3e64889611d8a5bea6adc6d98b35e0d066e278
mitchellpound <mitchell.pound@usu.edu>
2023-10-01 20:11:49 -0600

    Finished additions to income

--- fiscalsim_us/variables/gov/states/co/tax/income/additions/co_itemized_deduction_addback.py
+++ fiscalsim_us/variables/gov/states/co/tax/income/additions/co_itemized_deduction_addback.py
@@ -12,4 +12,18 @@ class co_itemized_deduction_addback(Variable):
     defined_for = StateCode.CO
 
     def formula(tax_unit, period, parameters):
-        pass
+       p = parameters(period).gov.states.co.tax.income.additions
+       filing_status = tax_unit("filing_status", period)
+       agi = tax_unit("adjusted_gross_income", period)
+
+       itemizing = tax_unit("tax_unit_itemizes", period)
+       itemized_deducts = tax_unit("taxable_income_deductions_if_itemizing", period)
+
+       income_required = agi >= p.itemized_deduciton_addback_income_threshold[filing_status]
+       deducts_required = itemized_deducts >= p.itemized_deduction_threshold[filing_status]
+
+       where(
+           income_required * deducts_required,
+           itemized_deducts - p.itemized_deduction_addback_subtraction[filing_status],
+           0
+             )
