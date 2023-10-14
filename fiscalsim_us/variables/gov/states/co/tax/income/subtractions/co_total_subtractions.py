d51df4ec0a62bb8263c279d1776c76c17191b483
mitchellpound <mitchell.pound@usu.edu>
2023-10-14 13:06:52 -0600

    Minor adjustments to co subtractions

--- fiscalsim_us/variables/gov/states/co/tax/income/subtractions/co_total_subtractions.py
+++ fiscalsim_us/variables/gov/states/co/tax/income/subtractions/co_total_subtractions.py
@@ -11,7 +11,9 @@ class co_total_subtractions(Variable):
     )
     defined_for = StateCode.CO
     adds = [
+        # state income tax refund line 1 Schedule 1 Form 1040
         "us_govt_interest",
-
-        "taxable_pension_income"
+        "co_social_security_subtraction",
+        "co_pension_subtraction",
+        "co_other_subtractions"
     ]
